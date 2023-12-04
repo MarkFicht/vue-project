68106ee0f2c904c49f3db8719d2c585f91b391c9
Marek <marekficht@gmail.com>
2023-12-04 15:58:56 +0100

    Fixed responsive for TheInLobby component + created that component

--- src/components/TheGame.vue
+++ src/components/TheGame.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import TheCardComponent from '@/components/TheCardComponent.vue';
+import TheInLobby from '@/components/TheInLobby.vue';
 import { useRouter } from 'vue-router';
-import LoadingSpinner from '@/components/LoadingSpinner.vue';
 import type IUser from '@/interfaces/User';
 import { inject, watch, onBeforeMount, onBeforeUnmount, ref } from 'vue';
 import { gameStore } from '@/store/GameStore';
@@ -24,11 +24,6 @@ const props = defineProps<{
     currentUser: IUser;
 }>();
 const { colors } = inject<any>('indicatorNavi');
-
-const acceptBtn = ref<string>('Accept');
-const cancelBtn = ref<string>('Cancel');
-const labelRedirect = ref<string>('Redirect for a few sec...');
-const labelWaiting = ref<string>('Approval game: ');
 
 const storeGame = gameStore();
 const { duel, gems, reflex } = storeToRefs(storeGame);
@@ -234,111 +229,18 @@ async function addAndRemoveToLobby(selectedGame: IGame['id']): Promise<any> {
         }
     }
 }
-
-// TODO - foo for other games in lobby
-async function acceptDuelGame(): Promise<any> {
-    if (duel.value.players.length === 2) {
-        if (duel.value.players.find((user) => user.uid === props.currentUser.uid)) {
-            await updateDoc(doc(usersRef, props.currentUser.uid), {
-                readyToGame: true
-            });
-            const newPlayers = duel.value.players.map((user) => {
-                return user.uid === props.currentUser.uid
-                    ? { ...user, readyToGame: true }
-                    : { ...user };
-            });
-            await updateDoc(gameStatusDuelRef, {
-                players: newPlayers
-            });
-        }
-    }
-}
-
-async function cancelDuelGame(): Promise<any> {
-    if (duel.value.players.length === 2) {
-        if (duel.value.players.find((user) => user.uid === props.currentUser.uid)) {
-            duel.value.players.forEach(async ({ uid }) => {
-                if (uid === props.currentUser.uid) {
-                    await updateDoc(doc(usersRef, uid), {
-                        game: '',
-                        readyToGame: false
-                    });
-                } else {
-                    await updateDoc(doc(usersRef, uid), {
-                        readyToGame: false
-                    });
-                }
-            });
-
-            const newPlayers = duel.value.players
-                .filter((user) => {
-                    return user.uid !== props.currentUser.uid ? user : null;
-                })
-                .map((user) => {
-                    return { ...user, readyToGame: false };
-                });
-
-            await updateDoc(gameStatusDuelRef, {
-                players: newPlayers
-            });
-        }
-    }
-}
 </script>
 
 <template>
     <section class="gameContainer">
         <!-- LOADING FOR PLAYERS CONTAINTERS -->
-        <div
+        <TheInLobby
             v-if="
                 duel.players.length === 2 &&
                 duel.players.find((user) => user.uid === currentUser.uid)
             "
-            class="infoAboutPlayers"
-        >
-            <p>
-                {{ duel.isStarted ? labelRedirect : labelWaiting + 'Duel' }}
-            </p>
-            <div>
-                <p>{{ duel.players[0].displayName || duel.players[0].email }}{{ ': ' }}</p>
-                <LoadingSpinner
-                    v-if="!duel.players[0].readyToGame"
-                    small
-                    :style="'margin-left: 10px;'"
-                />
-                <ion-icon
-                    v-else
-                    class="animateHand"
-                    name="thumbs-up-sharp"
-                    :style="'margin-left: 10px;'"
-                ></ion-icon>
-            </div>
-            <div>
-                <p>{{ duel.players[1].displayName || duel.players[1].email }}{{ ': ' }}</p>
-                <LoadingSpinner
-                    v-if="!duel.players[1].readyToGame"
-                    small
-                    :style="'margin-left: 10px;'"
-                />
-                <ion-icon
-                    v-else
-                    class="animateHand"
-                    name="thumbs-up-sharp"
-                    :style="'margin-left: 10px;'"
-                ></ion-icon>
-            </div>
-            <div
-                v-if="!duel.players.find((user) => user.uid === currentUser.uid)?.readyToGame"
-                class="containerApproveButtons"
-            >
-                <button @click="acceptDuelGame">
-                    {{ acceptBtn }}
-                </button>
-                <button @click="cancelDuelGame">
-                    {{ cancelBtn }}
-                </button>
-            </div>
-        </div>
+            :currentUser="currentUser"
+        />
 
         <!-- GAME CARDS CONTAINTERS -->
         <TheCardComponent
@@ -382,121 +284,5 @@ async function cancelDuelGame(): Promise<any> {
     align-items: center;
     overflow-x: hidden;
     overflow-y: scroll;
-}
-/* --- Lobby with full party --- */
-.infoAboutPlayers {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    z-index: 10;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background-color: rgba(100, 50, 150, 0.93);
-    color: #eee;
-    border-radius: 19px;
-    transition: all 0.5s;
-    animation: showElement 0.7s linear;
-}
-.infoAboutPlayers > p {
-    font-size: 2.2em !important;
-    letter-spacing: 2px;
-    font-weight: bold;
-    padding: 0 20px;
-    text-align: center;
-}
-.infoAboutPlayers > div {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    margin-top: 25px;
-    font-size: 1.7em !important;
-    letter-spacing: 1px;
-}
-.cardButton:hover {
-    letter-spacing: 0.25em;
-    background-color: tomato;
-}
-.containerApproveButtons {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 360px;
-    height: 50px;
-}
-.containerApproveButtons > button {
-    border: none;
-    top: 0;
-    height: 50px;
-    padding: 8px 30px;
-    position: absolute;
-    bottom: -30px;
-    border: none;
-    outline: none;
-    border-radius: 30px;
-    color: #fff;
-    background-color: var(--clr);
-    font-size: 1em;
-    letter-spacing: 0.1em;
-    text-transform: uppercase;
-    text-decoration: none;
-    transition: all 0.5s;
-    cursor: pointer;
-    box-shadow:
-        12px 12px 16px 0 rgba(255, 255, 255, 0.3) inset,
-        -8px -8px 12px 0 rgba(0, 0, 0, 0.25) inset;
-}
-.containerApproveButtons > button:hover {
-    letter-spacing: 0.25em;
-    background-color: tomato;
-}
-.containerApproveButtons button:first-child {
-    left: 5px;
-    font-size: 0.8em;
-}
-.containerApproveButtons button:last-child {
-    right: 5px;
-    font-size: 0.8em;
-}
-.animateHand {
-    animation: animateHand 1.5s infinite ease-in-out;
-}
-
-@media (max-width: 1024px) {
-    .infoAboutPlayers > p {
-        font-size: 1.9em !important;
-        letter-spacing: 1px;
-        font-weight: bold;
-    }
-    .infoAboutPlayers > div {
-        margin-top: 20px;
-        font-size: 1.5em !important;
-        letter-spacing: 1px;
-    }
-}
-@media (max-width: 720px) {
-    .infoAboutPlayers > p {
-        font-size: 1.7em !important;
-        letter-spacing: 1px;
-        font-weight: bold;
-    }
-    .infoAboutPlayers > div {
-        margin-top: 20px;
-        font-size: 1.2em !important;
-        letter-spacing: 1px;
-    }
-
-    .containerApproveButtons {
-        width: 250px;
-    }
-    .containerApproveButtons > button {
-        height: 40px;
-        padding: 8px 16px;
-    }
 }
 </style>
---
+++ src/components/TheInLobby.vue
@@ -0,0 +1,263 @@
+<script setup lang="ts">
+import LoadingSpinner from '@/components/LoadingSpinner.vue';
+import type IUser from '@/interfaces/User';
+import { ref } from 'vue';
+import { gameStore } from '@/store/GameStore';
+import { storeToRefs } from 'pinia';
+import {
+    usersRef,
+    gameStatusDuelRef,
+    gameStatusGemsRef,
+    gameStatusReflexRef
+} from '@/helpers/HelpersFirebaseConst';
+import { doc, updateDoc, serverTimestamp } from 'firebase/firestore';
+
+const props = defineProps<{
+    currentUser: IUser;
+}>();
+
+const acceptBtn = ref<string>('Accept');
+const cancelBtn = ref<string>('Cancel');
+const labelRedirect = ref<string>('Redirect for a few sec...');
+const labelWaiting = ref<string>('Approval game: ');
+
+const storeGame = gameStore();
+const { duel, gems, reflex } = storeToRefs(storeGame);
+
+// TODO timers with timestamp + remove away/offline players from lobby + set 30s from timestamp
+// TODO remove players and game if all players Disconnected/away
+
+// TODO - foo for other games in lobby
+async function acceptDuelGame(): Promise<any> {
+    if (duel.value.players.length === 2) {
+        if (duel.value.players.find((user) => user.uid === props.currentUser.uid)) {
+            await updateDoc(doc(usersRef, props.currentUser.uid), {
+                readyToGame: true
+            });
+            const newPlayers = duel.value.players.map((user) => {
+                return user.uid === props.currentUser.uid
+                    ? { ...user, readyToGame: true }
+                    : { ...user };
+            });
+            await updateDoc(gameStatusDuelRef, {
+                players: newPlayers
+            });
+        }
+    }
+}
+
+async function cancelDuelGame(): Promise<any> {
+    if (duel.value.players.length === 2) {
+        if (duel.value.players.find((user) => user.uid === props.currentUser.uid)) {
+            duel.value.players.forEach(async ({ uid }) => {
+                if (uid === props.currentUser.uid) {
+                    await updateDoc(doc(usersRef, uid), {
+                        game: '',
+                        readyToGame: false
+                    });
+                } else {
+                    await updateDoc(doc(usersRef, uid), {
+                        readyToGame: false
+                    });
+                }
+            });
+
+            const newPlayers = duel.value.players
+                .filter((user) => {
+                    return user.uid !== props.currentUser.uid ? user : null;
+                })
+                .map((user) => {
+                    return { ...user, readyToGame: false };
+                });
+
+            await updateDoc(gameStatusDuelRef, {
+                players: newPlayers
+            });
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="inLobbyPlayersContainer">
+        <h3>
+            {{ duel.isStarted ? labelRedirect : labelWaiting + 'Duel' }}
+        </h3>
+
+        <div>
+            <p>{{ duel.players[0].displayName || duel.players[0].email }}{{ ': ' }}</p>
+            <LoadingSpinner
+                v-if="!duel.players[0].readyToGame"
+                small
+                :style="'margin-left: 10px;'"
+            />
+            <ion-icon
+                v-else
+                class="animateHand"
+                name="thumbs-up-sharp"
+                :style="'margin-left: 10px;'"
+            ></ion-icon>
+        </div>
+
+        <div>
+            <p>{{ duel.players[1].displayName || duel.players[1].email }}{{ ': ' }}</p>
+            <LoadingSpinner
+                v-if="!duel.players[1].readyToGame"
+                small
+                :style="'margin-left: 10px;'"
+            />
+            <ion-icon
+                v-else
+                class="animateHand"
+                name="thumbs-up-sharp"
+                :style="'margin-left: 10px;'"
+            ></ion-icon>
+        </div>
+
+        <div
+            v-if="!duel.players.find((user) => user.uid === currentUser.uid)?.readyToGame"
+            class="ApproveButtonsContainer"
+        >
+            <button @click="acceptDuelGame">
+                {{ acceptBtn }}
+            </button>
+            <button @click="cancelDuelGame">
+                {{ cancelBtn }}
+            </button>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+/* --- Lobby with full party --- */
+.inLobbyPlayersContainer {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background-color: rgba(100, 50, 150, 0.93);
+    color: #eee;
+    border-radius: 19px;
+    transition: all 0.5s;
+    animation: showElement 0.7s linear;
+}
+.inLobbyPlayersContainer > h3 {
+    font-size: 20px;
+    min-height: 20px;
+    line-height: 20px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    padding: 0 10px;
+    text-align: center;
+}
+.inLobbyPlayersContainer > div {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-top: 25px;
+    letter-spacing: 1px;
+}
+.inLobbyPlayersContainer > div > p {
+    font-size: 14px;
+}
+/* --- Buttons --- */
+.ApproveButtonsContainer {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+}
+.ApproveButtonsContainer > button {
+    border: none;
+    height: 40px;
+    padding: 6px 10px;
+    margin: 0 20px;
+    border: none;
+    outline: none;
+    border-radius: 30px;
+    color: #fff;
+    background-color: var(--clr);
+    font-size: 14px;
+    line-height: 14px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    text-transform: uppercase;
+    text-decoration: none;
+    transition: all 0.5s;
+    cursor: pointer;
+    box-shadow:
+        12px 12px 16px 0 rgba(255, 255, 255, 0.22) inset,
+        -8px -8px 12px 0 rgba(0, 0, 0, 0.18) inset;
+}
+.ApproveButtonsContainer > button:last-child {
+    color: #ccc;
+}
+.ApproveButtonsContainer > button:hover {
+    transform: scale(1.1);
+    opacity: 0.9;
+}
+.animateHand {
+    animation: animateHand 1.5s infinite ease-in-out;
+}
+
+@media (min-width: 400px) {
+    .inLobbyPlayersContainer > h3 {
+        font-size: 24px;
+        min-height: 24px;
+        line-height: 24px;
+        padding: 0 20px;
+    }
+
+    .inLobbyPlayersContainer > div {
+        margin-top: 30px;
+    }
+
+    .inLobbyPlayersContainer > div > p {
+        font-size: 16px;
+    }
+    .ApproveButtonsContainer > button {
+        padding: 8px 16px;
+    }
+}
+
+@media (min-width: 720px) {
+    .inLobbyPlayersContainer > h3 {
+        font-size: 28px;
+        min-height: 28px;
+        line-height: 28px;
+    }
+
+    .inLobbyPlayersContainer > div > p {
+        font-size: 18px;
+    }
+    .ApproveButtonsContainer > button {
+        font-size: 16px;
+        line-height: 16px;
+    }
+}
+
+@media (min-width: 1024px) {
+    .inLobbyPlayersContainer > h3 {
+        font-size: 30px;
+        min-height: 30px;
+        line-height: 30px;
+    }
+
+    .inLobbyPlayersContainer > div > p {
+        font-size: 20px;
+    }
+    .ApproveButtonsContainer > button {
+        font-size: 18px;
+        line-height: 18px;
+    }
+}
+</style>
